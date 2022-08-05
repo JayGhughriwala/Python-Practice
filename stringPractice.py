e426f9986e852f57964911e3a3ba0a5c8c65860e
Jay <jayghughri1@gmail.com>
2022-08-05 17:40:05 +0530

    slicing Practice

---
+++ stringPractice.py
@@ -0,0 +1,17 @@
+str = "Jay Ghughriwala"
+#print(str[0:]) #This print the whole Line
+
+# For Reversing the string we required to only this trick
+# print(str[::-1])
+
+# print(str.endswith("Ghughriwala"))
+# print(str.count("J"))
+# print(str.capitalize())
+# print(str.find("Ghu"))
+
+# print(str.lower())
+
+#print(str.upper())
+#print(str.capitalize())
+print(str.replace("Jay","jay"))
+
