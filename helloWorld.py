9b178be3fa601b147ca295db9d2e46f39dec6ac3
Jay <jayghughri1@gmail.com>
2022-08-05 13:08:01 +0530

    without new line method

--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,8 @@ print('Hello World This is my first Python Program \n')
 # Hash use for single line comment
 """" Triple Quote use for 
         multi line comment"""
+
+# Now in python automatically new line was created when print function was created
+# without the new line this method was used
+print("This is a first line ", end="")
+print(" This is print without the new line")
