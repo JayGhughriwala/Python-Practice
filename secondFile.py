d5e8c7ff6516e99593fa438e75ad4fa8e935a060
Jay <jayghughri1@gmail.com>
2022-08-05 13:31:45 +0530

    Var Practice

--- secondFile.py
+++ secondFile.py
@@ -1,7 +1,10 @@
 var1 = 434;
 var2 = 45;
 
-var3="45"
-var4="83"
+var3 = "45"
+var4 = "83"
 print(int(var3) + int(var4))
-print(var1+var2)
+print(var1 + var2)
+
+# Now for printing any line for any times
+print(100 * "Jay\n")
