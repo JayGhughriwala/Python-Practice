28528bce8dc5dc041feb0832b4d38e7d52ad62c8
Jay <jayghughri1@gmail.com>
2022-08-06 18:31:13 +0530

    List & Tuple Practice

---
+++ dictionaryPractice.py
@@ -0,0 +1,25 @@
+dic1 = {}
+# print(type(dic1))
+dic2 = {"Name":"Jay", "Age":19, "City":"Surat"}
+# print(dic2)
+# print(dic2["Name"])
+
+#  Now nested dictionary
+dic3 = {"Name":{"First":"Jay", "Last":"Ghughriwala"}, "Age":19, "City":"Surat"}
+# print(dic3["Name"]["First"])
+# print((dic3["Name"]["Last"]))
+
+#  Now we can also add in that
+dic3["Name"]["Middle"] = "PrakashBhai"
+
+# print(dic3)
+#   Now removing a City from the dictionary
+# del dic3["City"]
+# print(dic3)
+
+# dic = {"Prank":"Fun", "Fun":"Prank"}
+#
+# print("Enter a word to find the synonym of that -- > ");
+# word = input()
+# print(dic[word])
+
---
+++ set.py
@@ -0,0 +1,12 @@
+s=set([1,12,3,4,6,1,6])
+# print(type(s))
+
+# list_from_set = [1,23,4,5,6,4,12,1,23]
+# print(list_from_set)
+# s.add(454)
+# print(s)
+# s.add(1)
+#  set not contain duplicate values
+# print(s)
+print(min(s))
+print(max(s))
