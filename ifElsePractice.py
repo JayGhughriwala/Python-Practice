8b9660f18bda39ec545bfd2e5d98dbfaddcf9575
Jay <jayghughri1@gmail.com>
2022-08-07 13:54:32 +0530

    If Else Practice

---
+++ ifElsePractice.py
@@ -0,0 +1,42 @@
+# print("This is a if else practice")
+# var1 = int(input("Enter a number "))
+# if var1>0:
+#     print("The number is positive")
+# elif var1==0:
+#     print("The number is zero")
+# else:
+#     print("The number is negative")
+
+#  Now practice the some keyword
+
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+#
+# #  take input from the user
+#
+# value = int(input("Enter a Value that you want to search"))
+#
+#
+# if value in list1:
+#     print(value, " is in the list")
+# elif value not in list1:
+#     print(value, " is not in the list")
+# else:
+#     print("You need to configure the list ")
+
+#  Now create a basic driving program
+
+# age = int(input("Enter Your age --> "))
+#
+# if age > 18 && age <= 90:
+#     print("You are eligible to drive a vehicle")
+# elif age > 90:
+#     print("You are too old to drive a vehicle , so my advice is to live a life , spend your savings and enjoy your life")
+# elif age == 0:
+#     print("You are not born yet, so my advice is to wait for your birthday")
+# elif age <= 10:
+#     print("You are too young to drive a vehicle, so my advice is to drive a bicycle and grow your physical strength")
+# elif age > 10 & age <= 18:
+#     print("You are not available to drive a vehicle")
+# else:
+#     print("You are not eligible to drive a vehicle")
+
