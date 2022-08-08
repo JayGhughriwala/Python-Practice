03cd7f64e5433e4a96f115e068deb2c72d20edb8
Jay <jayghughri1@gmail.com>
2022-08-08 18:15:14 +0530

    Operator practice

---
+++ Operator_practice.py
@@ -0,0 +1,23 @@
+print("This is a a operator practice ")
+print("The first section is the practice of the assignment operator")
+print(" 10 + 2 ", 10 + 2)
+print(" 10 - 2 ", 10 - 2)
+print(" 10 * 2 ", 10 * 2)
+print(" 11 / 2 ", 11 / 2)  # this will return a float vclue
+print(" 11 // 2 ", 11 // 2)  # this will return an integer value
+print(" 11 % 2 ", 11 % 2)  # this will return the remainder of the division
+print(" 11 ** 2 ", 11 ** 2)  # this will return the power of the number
+
+
+# Identity operator
+print("The second section is the practice of the identity operator")
+
+print(" 10 is 10 ", 10 is 10)
+print(" 10 is not 10 ", 10 is not 10)
+
+
+
+print("The third section is the practice of the membership operator")
+
+print(" 10 in [1,2,3,4,4,5,7,5,3,1] ", 10 in [1, 2, 3, 4, 4, 5, 7, 5, 3, 1])
+print(" 10 not in [1,2,3,4,4,5,7,5,3,1] ", 10 not in [1, 2, 3, 4, 4, 5, 7, 5, 3, 1])
