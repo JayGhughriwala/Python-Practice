ec5ea5c8342feae577e990c7ab1863c2759a4ab8
Jay <jayghughri1@gmail.com>
2022-08-09 10:44:49 +0530

    if else shorthand practice

---
+++ ifElseShortHand.py
@@ -0,0 +1,5 @@
+print("This is the practice of if else short hand")
+num1 = int(input("Enter the first number :- "))
+num2 = int(input("Enter the second number :- "))
+
+print(" num 1 is greater than num2 ") if num1 > num2 else print("num1 is less than num2")
