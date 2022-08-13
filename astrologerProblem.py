faa120c930e24692675d516f540be32b1ade07d0
Jay <jayghughri1@gmail.com>
2022-08-13 18:18:04 +0530

    Pattern with reverse

--- astrologerProblem.py
+++ astrologerProblem.py
@@ -11,9 +11,22 @@ print("Hello " + name + " , the program is starting ")
 # ***
 # ****
 # Pattern like this
-
-for i in range(n):
-    for j in range(i+1):
-        print(" * ",end="")
-
+inp =input("input 1 for pattern as it is or input 0 for pattern reverse --> ?")
+if inp == 1:
+    for i in range(n):
+        for j in range(i+1):
+            print(" * ",end="")
     print(" ")
+#
+#   in else pattern like this
+
+#  * * * * *
+#  * * * *
+#  * * *
+#  * *
+#  *
+else:
+    for i in range(n,0,-1):
+        for j in range(0,i):
+            print(" * ",end="")
+        print("")
