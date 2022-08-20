9a0fc0638a13b15fdd74154f9dcfebd28c6e9c7c
Jay <jayghughri1@gmail.com>
2022-08-20 16:48:08 +0530

    time Module Practice

---
+++ timemodule.py
@@ -0,0 +1,23 @@
+import time
+
+# start = time.time()
+#
+# for i in range(1000):
+#     pass
+# print(time.time() - start)
+#
+# print("While loop")
+#
+# start = time.time()
+# i = 0
+# while i < 10000:
+#     i += 1
+# print(time.time() - start)
+
+# print("The Local time is ", time.asctime())
+
+print("Now practice of the time.sleep function")
+
+print("Now program is sleep for a 2 seconds ")
+time.sleep(2)
+print("Now program is rerun after 2 seconds")
